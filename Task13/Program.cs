72f469f504dc0727f7dd656fa1a82c67314dbc62
Igor <eigsn@yandex.ru>
2023-01-23 00:21:39 +0300

    Task13 debaging

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,10 +1,18 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-//645 -> 5
-//78 -> третьей цифры нет
-//32679 -> 6
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
 
-
-
-
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число:");
+//int num=Convert.ToInt32(Console.ReadLine());
+int num=Convert.ToInt32(Console.ReadLine());
+if (num>99)
+    {
+    string txt=Convert.ToString(num);
+    Console.WriteLine("Вывод (текстовый)"+ txt.Length +" "+ txt[2]);
+    //Console.WriteLine($,"Вывод" {txt});
+    //int p=Convert.ToInt32((Math.Log10(num))%1);
+    int p=(int)((Math.Log10(num)))+1;
+    Console.WriteLine("Вывод "+ p);
+    }
+else Console.WriteLine("третьей цифры нет");
