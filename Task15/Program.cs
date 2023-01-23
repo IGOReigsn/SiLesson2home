0af28cccabc8c7cda09e92a9f88bcdeaffcd0473
IGOReigsn <eigsn@yandex.ru>
2023-01-23 14:53:18 +0300

    Tasks 10,15 - OK

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,5 +3,15 @@
 //7 -> да
 //1 -> нет
 
+// Чтобы не злоупотреблять "спойлером", полученным на семинаре 3- сделано через строковую переменную 
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+string? txt=Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+if (txt!.Length>1)
+    {Console.WriteLine("Неправильный ввод - нужна одна цифра");}
+else if (txt[0]=='1' || txt[0]=='2'|| txt[0]=='3' || txt[0]=='4'  || txt[0]=='5')
+    {Console.WriteLine("Нет");}
+else if (txt[0]=='6' || txt[0]=='7' )
+    {Console.WriteLine("ДА");}  
+else 
+    {Console.WriteLine("Такого дня не существует");}
